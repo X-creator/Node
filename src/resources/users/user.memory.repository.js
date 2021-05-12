36975285bfa890d32150f04978a4db9be229050b
X-creator <farengeyt12@mail.ru>
2021-05-12 19:07:21 +0500

    init: task2

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,132 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+const users = [
+  {
+    "id": "609bdad1fc13ae46fb000000",
+    "name": "Aida Ridolfo",
+    "login": "recontextualize",
+    "password": "XxhRB9PNN"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000001",
+    "name": "Darcy Pavluk",
+    "login": "utilizesynergistic",
+    "password": "QtVLqCIVxuo"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000002",
+    "name": "Toni Hosburn",
+    "login": "monetizewireless",
+    "password": "DDIvaPsfO"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000003",
+    "name": "Obed Southers",
+    "login": "expediteseamless",
+    "password": "jY2BJ6TB"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000004",
+    "name": "Delmor Mee",
+    "login": "cultivateB2C",
+    "password": "5TyClfI9tpW"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000005",
+    "name": "Bliss Pallatina",
+    "login": "brandvisionary",
+    "password": "EsaitXh"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000006",
+    "name": "Tatiania McElane",
+    "login": "harnessscalable",
+    "password": "WAWYDwn4k"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000007",
+    "name": "Vanny Andrasch",
+    "login": "implementsynergistic",
+    "password": "6G7z9Vh8BSns"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000008",
+    "name": "Christal Jobes",
+    "login": "matrixproactive",
+    "password": "87d3cTQu9Neo"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000009",
+    "name": "Harmon Keyho",
+    "login": "e-enable24",
+    "password": "qJnOPLjKHlRL"
+  },
+  {
+    "id": "609bdad1fc13ae46fb00000a",
+    "name": "Read Grouer",
+    "login": "enhanceone",
+    "password": "PAM2Es7On8"
+  },
+  {
+    "id": "609bdad1fc13ae46fb00000b",
+    "name": "Cornela Crambie",
+    "login": "repurposetrans",
+    "password": "rFzUq8bfEt"
+  },
+  {
+    "id": "609bdad1fc13ae46fb00000c",
+    "name": "Lorilyn Snodden",
+    "login": "repurposeback",
+    "password": "bDHcNtk"
+  },
+  {
+    "id": "609bdad1fc13ae46fb00000d",
+    "name": "Misha De Normanville",
+    "login": "optimizeend",
+    "password": "XUEqFti1r"
+  },
+  {
+    "id": "609bdad1fc13ae46fb00000e",
+    "name": "Pier McCrudden",
+    "login": "iterateB2B",
+    "password": "IElFHFX"
+  },
+  {
+    "id": "609bdad1fc13ae46fb00000f",
+    "name": "Thea Phizaclea",
+    "login": "expeditedistributed",
+    "password": "pvZNH1IpD"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000010",
+    "name": "Napoleon Kensley",
+    "login": "evolveworld",
+    "password": "qVZZzuKt"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000011",
+    "name": "Anatole Groneway",
+    "login": "evolveB2C",
+    "password": "J790XX"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000012",
+    "name": "Carlene Mackett",
+    "login": "synergizeim",
+    "password": "BQtWoUYa"
+  },
+  {
+    "id": "609bdad1fc13ae46fb000013",
+    "name": "Vitoria Farran",
+    "login": "syndicateholi",
+    "password": "3zr5JwJRTWi"
+  }
+];
 
-module.exports = { getAll };
+const getAll = async () => users;
+const getByID = async () => {
+
+};
+const createOne = async () => users;
+const updateOne = async () => users;
+const deleteOne = async () => users;
+
+module.exports = { getAll, getByID, createOne, updateOne, deleteOne };
