180db326a5f2fd39053b345531951af8c808059f
X-creator <farengeyt12@mail.ru>
2021-05-14 13:01:23 +0500

    feat: done users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,4 +1,4 @@
-const users = [
+let users = [
   {
     "id": "609bdad1fc13ae46fb000000",
     "name": "Aida Ridolfo",
@@ -58,75 +58,21 @@ const users = [
     "name": "Harmon Keyho",
     "login": "e-enable24",
     "password": "qJnOPLjKHlRL"
-  },
-  {
-    "id": "609bdad1fc13ae46fb00000a",
-    "name": "Read Grouer",
-    "login": "enhanceone",
-    "password": "PAM2Es7On8"
-  },
-  {
-    "id": "609bdad1fc13ae46fb00000b",
-    "name": "Cornela Crambie",
-    "login": "repurposetrans",
-    "password": "rFzUq8bfEt"
-  },
-  {
-    "id": "609bdad1fc13ae46fb00000c",
-    "name": "Lorilyn Snodden",
-    "login": "repurposeback",
-    "password": "bDHcNtk"
-  },
-  {
-    "id": "609bdad1fc13ae46fb00000d",
-    "name": "Misha De Normanville",
-    "login": "optimizeend",
-    "password": "XUEqFti1r"
-  },
-  {
-    "id": "609bdad1fc13ae46fb00000e",
-    "name": "Pier McCrudden",
-    "login": "iterateB2B",
-    "password": "IElFHFX"
-  },
-  {
-    "id": "609bdad1fc13ae46fb00000f",
-    "name": "Thea Phizaclea",
-    "login": "expeditedistributed",
-    "password": "pvZNH1IpD"
-  },
-  {
-    "id": "609bdad1fc13ae46fb000010",
-    "name": "Napoleon Kensley",
-    "login": "evolveworld",
-    "password": "qVZZzuKt"
-  },
-  {
-    "id": "609bdad1fc13ae46fb000011",
-    "name": "Anatole Groneway",
-    "login": "evolveB2C",
-    "password": "J790XX"
-  },
-  {
-    "id": "609bdad1fc13ae46fb000012",
-    "name": "Carlene Mackett",
-    "login": "synergizeim",
-    "password": "BQtWoUYa"
-  },
-  {
-    "id": "609bdad1fc13ae46fb000013",
-    "name": "Vitoria Farran",
-    "login": "syndicateholi",
-    "password": "3zr5JwJRTWi"
   }
 ];
 
 const getAll = async () => users;
-const getByID = async () => {
-
+const getByID = async (id) => users.find(user => user.id === id);
+const createOne = async (user) => {
+  users.push(user);
+  return user;
 };
-const createOne = async () => users;
-const updateOne = async () => users;
-const deleteOne = async () => users;
+const updateOne = async (data) => {
+  users = users.map(user => user.id === data.id ? data : user);
+  return users.find(user => user.id === data.id);
+};
+const deleteOne = async (id) => {
+  users = users.filter(user => user.id !== id);
+};
 
 module.exports = { getAll, getByID, createOne, updateOne, deleteOne };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,36 @@
-const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const router = require("express").Router();
+const User = require("./user.model");
+const usersService = require("./user.service");
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router.route("/")
+  .get(async (req, res) => {
+    const users = await usersService.getAll();
+    res.json(users.map(User.toResponse));
+  })
+  .post(async (req, res) => {
+    const { name, login, password } = req.body;
+    const user = new User({ name, login, password });
+    const createdUser = await usersService.createOne(user);
+    res.status(201).send(User.toResponse(createdUser));
+  });
+
+router.route("/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const user = await usersService.getByID(id);
+    res.json(User.toResponse(user));
+  })
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { name, login, password } = req.body;
+    const user = new User({ id, name, login, password });
+    const updatedUser = await usersService.updateOne(user);
+    res.json(User.toResponse(updatedUser));
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    await usersService.deleteOne(id);
+    res.sendStatus(204);
+  });
 
 module.exports = router;
